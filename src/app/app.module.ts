c2c5de631c19653a787c7f33e32833a4f0c93431
Bhushan Gandla <bhushan.gandla@svclnk.com>
2023-04-17 22:22:21 -0400

    Added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { GetInTouchComponent } from './get-in-touch/get-in-touch.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutMeComponent,
+    PortfolioComponent,
+    GetInTouchComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
